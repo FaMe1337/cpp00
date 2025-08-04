9010cfb72db4acef2626c5b7847035106ce13c0e
FaMe1337 <fabio4_8@hotmail.com>
2025-08-04 19:19:03 +0100

    starting

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: fabio <fabio@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 13:43:45 by fabio             #+#    #+#             */
-/*   Updated: 2025/08/04 19:09:34 by fabio            ###   ########.fr       */
+/*   Updated: 2025/08/04 19:18:33 by fabio            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int main(int argc, char** argv)
         {
             for (int j = 0; argv[i][j]; j++)
                 std::cout << (char)std::toupper(argv[i][j]);
+            std::cout << " ";
         }
         std::cout << "\n";
     }
